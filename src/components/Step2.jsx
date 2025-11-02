02edd4e3600ecbba4e3ff3ee7a3b3ef7ac37f011
nithinbairu <nithinbairu08@gmail.com>
2025-11-02 12:38:07 +0530

    1st commit

--- src/components/Step2.jsx
+++ src/components/Step2.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Step2 = ({ formData, handleChange }) => {
   return (
-    <>
+    <div id="step2">
         <h2>Car Details</h2>
         <input type='text' id="model" value={formData.model}
         onChange={handleChange}></input>
         <br></br>
         <input type='text' id="car_price" value={formData.car_price}
         onChange={handleChange}></input>
-    </>
+    </div>
   )
 }
 
--- src/components/Step3.jsx
+++ src/components/Step3.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Step3 = ({ formData, handleChange }) => {
   return (
-    <>
+    <div id="step2">
         <h2>Payment Details</h2>
         <input type='text' id="card_info" value={formData.card_info}
         onChange={handleChange}></input>
         <br></br>
         <input type='text' id="expiry_date" value={formData.expiry_date}
         onChange={handleChange}></input>
-    </>
+    </div>
   )
 }
 
